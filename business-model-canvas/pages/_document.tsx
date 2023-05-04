fbbd1c1165cd4b679f1e12936e67f6ba31b731c8
Charles <yqu39@wisc.edu>
2023-05-04 14:20:18 -0500

    feat: google analytics

--- business-model-canvas/pages/_document.tsx
+++ business-model-canvas/pages/_document.tsx
@@ -1,13 +1,30 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+          {/* Global site tag (gtag.js) - Google Analytics */}
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-GQ1WVD16MQ"
+            strategy="afterInteractive"
+          />
+          <Script
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-GQ1WVD16MQ');
+              `,
+            }}
+          />
+        </body>
+      </Html>
   )
 }
