d104ac294e4b266cc87b3f93f861266b49f0670b
charles <yqu39@wisc.edu>
2023-05-03 23:44:18 -0500

    feat: hello text

--- business-model-canvas/components/Container.tsx
+++ business-model-canvas/components/Container.tsx
@@ -1,35 +1,13 @@
 import React from 'react'
 
 type Props = {
-  colSpan?: number;
-  rowSpan?: number;
-  bgColor?: string;
   children: React.ReactNode;
-  colStart?: number;
-  colEnd?: number;
 }
 
-const Container = ( { colSpan, rowSpan, bgColor='bg-gray-300', children, colStart, colEnd } : Props) => {
-  let containerClass = `p-4 ${bgColor} rounded-md`
-
-  if (colSpan) {
-    containerClass += ` col-span-${colSpan}` 
-  }
-
-  if (rowSpan) {
-    containerClass += ` row-span-${rowSpan}`
-  }
-
-  if (colStart) {
-    containerClass += ` col-start-${colStart}`
-  }
-
-  if (colEnd) {
-    containerClass += ` col-end-${colEnd}` 
-  }
+const Container = ( { children } : Props) => {
 
   return ( 
-    <div className={containerClass}>
+    <div className='text-sm text-green-100'>
       {children}
     </div>
   );
--- business-model-canvas/pages/index.tsx
+++ business-model-canvas/pages/index.tsx
@@ -1,48 +1,76 @@
 import Header from "@/components/Header";
+import Container from "@/components/Container";
 
 export default function Home() {
-  const containerClass = `p-4 bg-gray-300 rounded-md`;
+  const containerClass = `p-4 bg-gray-300 rounded-md min-w-[100px] min-h-[120px] max-h-[600px]`;
 
   return (
     <div>
       <Header />
-      <div className="">
+      <div className="flex items-center justify-center">
         {/* Business Model Canvas */}
-        <div className="grid grid-cols-10 grid-rows-4 gap-4 p-8">
+        <div className="grid grid-cols-10 grid-flow-row gap-4 p-8">
           <div className={`col-span-2 row-span-2 ${containerClass}`}>
             <h3>Key Partnerships</h3>
+            <Container>
+              hello
+            </Container>
           </div>
 
           <div className={`col-span-2 row-span-1 ${containerClass}`}>
             <h3>Key Activities</h3>
+            <Container>
+              hello
+            </Container>
           </div>
 
           <div className={`col-span-2 row-span-2 ${containerClass}`}>
             <h3>Value Propositions</h3>
+            <Container>
+              hello
+            </Container>
           </div>
 
           <div className={`col-span-2 row-span-1 ${containerClass}`}>
             <h3>Customer Relationships</h3>
+            <Container>
+              hello
+            </Container>
           </div>
 
           <div className={`col-span-2 row-span-2 ${containerClass}`}>
             <h3>Customer Segments</h3>
+            <Container>
+              hello
+            </Container>
           </div>
 
           <div className={`col-span-2 row-span-1 ${containerClass}`}>
             <h3>Key Resources</h3>
+            <Container>
+              hello
+            </Container>
           </div>
 
           <div className={`col-span-2 row-span-1 ${containerClass}`}>
             <h3>Channels</h3>
+            <Container>
+              hello
+            </Container>
           </div>
 
           <div className={`col-span-5 row-span-1 ${containerClass}`}>
             <h3>Revenue Streams</h3>
+            <Container>
+              hello
+            </Container>
           </div>
 
           <div className={`col-span-5 row-span-1 ${containerClass}`}>
             <h3>Cost Structure</h3>
+            <Container>
+              hello
+            </Container>
           </div>
         </div>
       </div>
